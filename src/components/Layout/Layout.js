2a906b51a7356f5a762723e8340b87ade65b592f
kavindadilshan <kavindadilshan97@gmail.com>
2021-03-23 11:51:42 +0530

    create nav bar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AuxElement from '../../hoc/AuxElement';
 import Classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout =(props)=>(
     <AuxElement>
-        <div>Toolbar,SideDrawe,Backdrop</div>
+        <Toolbar/>
         <main className={Classes.Content}>
             {props.children}
         </main>
